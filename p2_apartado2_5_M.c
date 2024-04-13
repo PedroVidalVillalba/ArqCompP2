eefcd9bc9c398eefc20249856bc106ff43c883de
kaox <caolopezcarlos@gmail.com>
2024-04-13 17:56:07 +0200

    Apartado 2, ejercicio por bloques solucionado. p2_apartado2_5_M.c mismo programa, usaando matrices ejercicio 1 en vez de arrays. Mismo rendimiento

--- p2_apartado2_5_M.c
+++ p2_apartado2_5_M.c
@@ -7,26 +7,27 @@
  * @authors Vidal Villalba, Pedro
  */
 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 
 #define M 8
-
 #define RAND_SEED 888
+
 /* drand48 devuelve un double aleatorio en [0, 1); le sumamos 1 para ponerlo en [1, 2)
  * lrand48 devuelve un long aleatorio; si el número es par multiplicamos por 1 y si es impar por -1 */
 #define get_rand() ((2 * (lrand48() & 1) - 1) * (1 + drand48()))
 
-#define min(a, b) (a < b ? a : b)
 
-/* CÓDIGO ASOCIADO A LA MEDIDA DE CICLOS */
+/*** CÓDIGO ASOCIADO A LA MEDIDA DE CICLOS ***/
 
 void start_counter();
 double get_counter();
 
 /* Initialize the cycle counter */
+
 static unsigned cyc_hi = 0;
 static unsigned cyc_lo = 0;
 
@@ -68,26 +69,43 @@ double get_counter()
     return result;
 }
 
-/* FIN CÓDIGO ASOCIADO A LA MEDIDA DE CICLOS */
+/*** FIN CÓDIGO ASOCIADO A LA MEDIDA DE CICLOS ***/
 
-void random_matrix(double *matrix, int rows, int columns) {
-    int i, j;
-
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < columns; j++) {
-            matrix[i * columns + j] = get_rand();
-        }
+void alloc_matrix(double*** matrix, int rows, int columns) {
+    *matrix = (double **) malloc(rows * sizeof(double));
+    for (int i = 0; i < rows; i++) {
+        (*matrix)[i] = (double *) calloc(columns, sizeof(double));
     }
 }
 
-void random_array(double* array, int size) {
+void free_matrix(double **matrix, int rows, int columns) {   
     int i;
 
-    for (i = 0; i < size; i++) {
-        array[i] = get_rand();
+    for (i = 0; i < (rows); i++) {
+        free(matrix[i]);
     }
+    free(matrix);
 }
 
+void random_matrix(double **matrix, int rows, int columns) {  
+    int i, j;
+
+    for (i = 0; i < rows; i++) {                
+        for (j = 0; j < columns; j++) {         
+            matrix[i][j] = get_rand();          
+        }                                       
+    }                                           
+}
+
+void random_array(double* array, int size) {         
+    int i;
+
+    for (i = 0; i < size; i++) {            
+        array[i] = get_rand();                  
+    }                                           
+}
+
+/** Fisher-Yates shuffle */
 void random_index(int** index, int size) {
     int i, j;
     int temp;
@@ -108,15 +126,16 @@ void random_index(int** index, int size) {
 }
 
 int main(int argc, char** argv) {
-    int i, j, k; // Reordenamos datos 1
-    int ii, jj;
-    int N; // Reordenamos datos 2
-    double *a, *b, *d;
-    int *ind; // Reordenamos datos 3 (así esta cerca de d)
+    double **a, **b, **d;
     double *c, *e;
     double f;
     double ck;
+    int *ind;
+    int i, j, k;
+    int ii, jj;
+    int N;
     long line_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+
 
     /* Procesamos los argumentos */
     if (argc != 2) {
@@ -125,11 +144,11 @@ int main(int argc, char** argv) {
     }
     N = atoi(argv[1]);
 
-    a = (double *) malloc(N * M * sizeof(double));
-    b = (double *) malloc(M * N * sizeof(double));
+    alloc_matrix(&a, N, M);
+    alloc_matrix(&b, M, N);
     c = (double *) malloc(M * sizeof(double));
     e = (double *) malloc(N * sizeof(double));
-
+    
     srand48(RAND_SEED);
     random_matrix(a, N, M);
     random_matrix(b, M, N);
@@ -137,7 +156,7 @@ int main(int argc, char** argv) {
     random_index(&ind, N);
 
     // Inicialización de todas las componentes de d a cero
-    d = (double *) calloc(N * N, sizeof(double));
+    alloc_matrix(&d, N, N);
 
     // Comenzamos el contador de ciclos
     start_counter();
@@ -147,31 +166,29 @@ int main(int argc, char** argv) {
 
     for (i=0; i < N - block_size ; i+=block_size)
         for (j=0; j < N - block_size; j+=block_size)
-                for (ii=i; ii<i+block_size; ii++) {
-                    for (jj=j; jj<j+block_size; jj++){
-                        for (k=0; k < M; k++){
-                            d[ii * N + jj] += a[ii * M + k] * (b[k * N + jj] - c[k]);
-                        }
-                        d[ii * N + jj] *=2;
+            for (ii=i; ii<i+block_size; ii++) {
+                for (jj=j; jj<j+block_size; jj++){
+                    for (k=0; k < M; k++){
+                        d[ii][jj] += 2 * a[ii][k] * (b[k][jj] - c[k]);
                     }
-
                 }
+
+            }
 
 
     // Hacer operaciones restantes
     for(i = ii; i < N; i++) {
         for(j = jj; j < N; j++){
             for(k=0; k < M; k++){
-                d[i * N + j] += a[i * M + k] * (b[k * N + j] - c[k]);
+                d[i][j] += 2 * a[i][k] * (b[k][j] - c[k]);
             }
-            d[i * N + j] *=2;
         }
     }
 
     f = 0;
 
     for (i = 0; i < N; i++) {
-        e[i] = d[ind[i] * (N + 1)] / 2;
+        e[i] = d[ind[i]][ind[i]] / 2;
         f += e[i];
     }
 
@@ -182,13 +199,12 @@ int main(int argc, char** argv) {
 
     printf("\n Clocks=%1.10lf \n",ck);
 
-    free(a);
-    free(b);
-    free(d);
+    free_matrix(a, N, M);
+    free_matrix(b, M, N);
+    free_matrix(d, N, N);
     free(c);
     free(e);
     free(ind);
 
     exit(EXIT_SUCCESS);
 }
-
